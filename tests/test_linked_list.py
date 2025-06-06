0b61b35f432896eac89c578c6cdec7d11048aa8b
irbbb <94206434+irbbb@users.noreply.github.com>
2025-06-06 23:32:41 +0200

    feature: added tests to linked list peek method

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -49,3 +49,27 @@ def test_reuse_after_empty():
     assert len(ll) == 0
     ll.prepend(2)
     assert ll.pop() == 2
+
+def test_peek_non_empty():
+    ll = LinkedList()
+    ll.prepend(5)
+    assert ll.peek() == 5
+    assert len(ll) == 1
+
+def test_peek_multiple():
+    ll = LinkedList()
+    ll.prepend(1)
+    ll.prepend(2)
+    ll.prepend(3)
+    assert ll.peek() == 3
+    assert len(ll) == 3
+
+def test_peek_empty():
+    ll = LinkedList()
+    assert ll.peek() is None
+
+def test_peek_after_pop():
+    ll = LinkedList()
+    ll.prepend(1)
+    ll.pop()
+    assert ll.peek() is None
